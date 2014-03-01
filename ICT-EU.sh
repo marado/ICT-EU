47785e8c94cfe538e40f31dd1e5734d17e25dc70
Marcos Marado <mindboosternoori@gmail.com>
2014-03-01 17:36:25 +0000

    Fetching is done, now we need to process the projects

--- ICT-EU.sh
+++ ICT-EU.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # 01 -- fetch list
-# TODO
+rm -rf tmp ;
+mkdir tmp ;
+cd tmp ;
+echo "Fetching projects list";
+wget "http://ec.europa.eu/information_society/apps/projects/index.cfm?menu=secondary" -O list.html -o /dev/null ;
 
 # 02 -- fetch projects
-# TODO
+for purl in `grep factsheet\/index list.html |cut -d\" -f2`; do 
+	pname=$(echo $purl|cut -d= -f2);
+	echo "Fetching project: $pname";
+	wget "http://ec.europa.eu/information_society/apps/projects/$purl" -O project-$pname -o /dev/null ;
+done
 
 # 03 -- for each project, grab info and process it
 # TODO
