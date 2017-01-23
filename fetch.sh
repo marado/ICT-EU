b5b3d2b2c619f9339f1f323c9f287c2ebbb71735
Marcos Marado <mindboosternoori@gmail.com>
2017-01-23 17:21:31 +0000

    fixed fetch url

--- fetch.sh
+++ fetch.sh
@@ -5,7 +5,7 @@ mkdir tmp ;
 cd tmp ;
 echo "Fetching projects:";
 
-let pages=($(wget "http://cordis.europa.eu/projects/result_en?q=%28contenttype%3D%27project%27+OR+%2Fresult%2Frelations%2Fcategories%2FresultCategory%2Fcode%3D%27brief%27%2C%27report%27%29+AND+programme%2Fpga%3D%27FP7-ICT%27&p=$page&num=100" -o /dev/null -O -|grep "Results 1 - 100 of"|cut -df -f2|sed 's/\ //g')+99)/100
+let pages=($(wget "http://cordis.europa.eu/projects/result_en?q=%28contenttype%3D%27project%27+OR+%2Fresult%2Frelations%2Fcategories%2FresultCategory%2Fcode%3D%27brief%27%2C%27report%27%29+AND+programme%2Fpga%3D%27FP7-ICT%27&p=$page&num=100&format=csv" -o /dev/null -O -|grep "Results 1 - 100 of"|cut -df -f2|sed 's/\ //g')+99)/100
 
 echo "There are $pages pages to fetch.";
 for page in $(seq 1 "$pages"); do
